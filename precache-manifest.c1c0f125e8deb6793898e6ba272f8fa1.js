9f8b12fc357c47f7ed79ed5eba12740f83b145c1
Roman Gladyshev <romagladyshev@gmail.com>
2020-09-16 11:27:00 +0600

    Updates

---
+++ precache-manifest.c1c0f125e8deb6793898e6ba272f8fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30983adfdfb2fee547802afe4efb4be",
+    "url": "/chat-client/index.html"
+  },
+  {
+    "revision": "7522d89239a0e1ae5784",
+    "url": "/chat-client/static/css/main.677d1df8.chunk.css"
+  },
+  {
+    "revision": "881ba08ea4e9b3d7b1c6",
+    "url": "/chat-client/static/js/2.df07fca3.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-client/static/js/2.df07fca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7522d89239a0e1ae5784",
+    "url": "/chat-client/static/js/main.0ca37fa2.chunk.js"
+  },
+  {
+    "revision": "657f21c60e4e84e33c1f",
+    "url": "/chat-client/static/js/runtime-main.6871a354.js"
+  }
+]);
